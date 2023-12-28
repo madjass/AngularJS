9bb718049cdee0bef5a45f89d13d7bfd7268bd06
madjass <madjao@gmail.com>
2023-12-27 22:39:12 -0600

    adding forms and validation for account component

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { UserForm } from "../form/user-form";
 
 @Component({
   selector: 'app-account',
@@ -7,8 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AccountComponent {
 
-  onSave(form: any){
-    console.log(form);
+//  form = new UserForm();
+
+    form: FormGroup = new FormGroup({
+      firstName : new FormControl<string>('', Validators.required),
+      lastName : new FormControl<string>('', Validators.required),
+      email : new FormControl<string>('', Validators.required),
+      address : new FormControl<string>('', Validators.required),
+      apt : new FormControl<number>(0),
+      city : new FormControl<string>('', Validators.required),
+      state : new FormControl<string>('', Validators.required),
+      zipCode : new FormControl<number>(0, Validators.required),
+      gender : new FormControl<string>(''),
+      dob : new FormControl<string>('', Validators.required),
+      phone : new FormControl<number>(0, Validators.required),
+    });
+
+  onSave(){
+  console.log(this.form);
   }
+
+
 
 }
---
+++ src/app/form/user-form.ts
@@ -0,0 +1,23 @@
+
+import {FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms'
+
+export class UserForm extends FormGroup {
+
+firstName : FormControl<string>('', Validators.required);
+lastName : FormControl<string>('', Validators.required);
+email : FormControl<string>('', Validators.required);
+address : FormControl<string>('', Validators.required);
+apt : FormControl<number>(0);
+city : FormControl<string>('', Validators.required);
+state : FormControl<string>('', Validators.required);
+zipCode : FormControl<number>(0, Validators.required);
+gender : FormControl<string>('');
+dob : FormControl<string>('', Validators.required);
+phone : FormControl<number>(0, Validators.required);
+
+contructor(builder: FormBuilder){
+  super(UserForm);
+
+}
+
+}
